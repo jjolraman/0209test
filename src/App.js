34fde201e35adebf8409ecef6ede8814e74dd7a9
jjolraman <jjolraman@gmail.com>
2023-02-09 10:56:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const Todo = () => {
+  const [todoTitle, setTodoTitle] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  const handleAddTodo = () => {
+    if (!todoTitle) return;
+    setTodos([
+      ...todos,
+      { id: Math.random() + Date(), title: todoTitle, isDone: false },
+    ]);
+    setTodoTitle("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <div className="todobox">
+        <div>
+          <input
+            type="text"
+            value={todoTitle}
+            onChange={(e) => setTodoTitle(e.target.value)}
+          />
+        </div>
+        <button onClick={handleAddTodo}>추가하기</button>
+      </div>
+
+      {todos
+        .filter((todo) => !todo.isDone)
+        .map((todo) => (
+          <div className="workingbox" key={todo.id}>
+            <h5>{todo.title}</h5>
+          </div>
+        ))}
     </div>
   );
-}
+};
 
-export default App;
+export default Todo;
